142e08e3ba9d6b8403167b75fd921f81854f2cb9
salavthapa <salav.thapa4@gmail.com>
2016-11-25 13:04:28 +0545

    Add files via upload

---
+++ logic operator.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Salav Thapa
+Subject:Programing Fundamental
+Roll:
+Program:to read a number from users
+Lab Sheet No:6
+Date:2016-11-25
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a=5,b=6;
+
+    if(a==5 || b==1)
+    {
+        printf("true");
+    }else{
+        printf("false");
+    }
+    return 0;
+
+}
